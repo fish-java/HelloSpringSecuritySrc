452f4e07a92da5fb19a3084d594a5d27eee230be
apple <wow@github.com>
2019-04-01 09:30:56 +0800

    add three imMemory User, and we can login with those users

--- src/main/java/com/github/fish56/hello_spring_security/config/WebConfig.java
+++ src/main/java/com/github/fish56/hello_spring_security/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.github.fish56.hello_spring_security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("super-admin")
+                .password("{noop}123456")
+                .roles("isUser","isAdmin")
+                .and()
+                .withUser("user")
+                .password("{noop}123456")
+                .roles("isUser")
+                .and()
+                .withUser("only-admin")
+                .password("{noop}123456")
+                .roles("isAdmin");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,6 +34,9 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
               .antMatchers("/users")
                 .hasRole("isAdmin")
               .antMatchers("/articles")
-                .hasRole("isUser");
+                .hasRole("isUser")
+                .and()
+              .formLogin()
+                .permitAll();
     }
 }
